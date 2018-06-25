2e456d8dd6caf066b4d2f2fe9098f7ca0088ec98
bartoszpieczynski <35772248+bartoszpieczynski@users.noreply.github.com>
2018-06-25 22:13:42 +0200

    import axios instance

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Wrapper from "../../hoc/Wrapper";
+import axios from "../../axios-orders";
 
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
